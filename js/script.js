901e394a9ad26bfe269c09ae6ccd88869be1fd7d
Sergey <example@gmail.com>
2022-08-16 15:17:13 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/* alert('hello');
+const result = confirm('тут?');
+alert(result); */
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваше фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers);
